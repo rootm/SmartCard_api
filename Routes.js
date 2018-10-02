1e8605e59203501826414baeb7406d147fd59960
rootm <muvindu.t@gmail.com>
2018-10-02 09:54:56 +0530

    journey endpoit route added

--- Routes.js
+++ Routes.js
@@ -1,9 +1,11 @@
 var express =require('express');
 var AccountRoute = require('./src/Account/Account.Route');
 var CardRoute = require('./src/Card/CardDetails.Route');
+var JourneyRoute = require('./src/Journey/Journey.Route');
 
 const Routes = express.Router();
 Routes.use('/accounts/',AccountRoute);
 Routes.use('/card/',CardRoute);
+Routes.use('/journey/', JourneyRoute);
 
 module.exports =Routes;
