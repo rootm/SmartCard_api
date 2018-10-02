12c19d02abd56e6697464f5712c4851e7fcd302d
rootm <muvindu.t@gmail.com>
2018-10-02 09:53:26 +0530

    journey endpoint files added and completed

---
+++ src/Journey/Journey.Controller.js
@@ -0,0 +1,41 @@
+var sequalize        = require('../DBSchema/SchemaMapper');
+const journeys = sequalize.customerJourney;
+
+var CardController = function() {
+
+    this.addJourney = ( data) => {
+        return new Promise((resolve, reject) => {
+            journeys.create(
+                {
+                    accountId: data.accountId,
+                    routeId: data.routeId,
+                    startStation: data.start,
+                    endStation: data.end,
+                    date: data.date,
+                    startTime: data.startTime,
+                    endTime: data.endTime,
+                    cost: data.cost
+                }
+            ).then(() => {
+                resolve({status: 200, message: "Successfully updated Balance"});
+            }).catch(err => {
+                reject({status: 500, message: "Error:- " + err});
+            })
+        })
+    }
+
+
+    this.getJourneys = (id) => {
+        return new Promise((resolve, reject) => {
+            journeys.findAll({where: {accountId: id}}).then(journey => {
+                resolve({status: 200, data: journey});
+            }).catch(err => {
+                reject({status: 500, message: "Error:- " + err});
+            })
+        })
+    }
+
+
+
+};
+module.exports = new CardController();
---
+++ src/Journey/Journey.Route.js
@@ -0,0 +1,26 @@
+var express     = require('express');
+var router      = express.Router();
+var Controller  = require('./Journey.Controller');
+
+router.put('/', (req, res) => {
+    Controller.addJourney(req.body).then(data => {
+        res.status(data.status).send({message: data.message});
+    }).catch(err => {
+        res.status(err.status).send({message: err.message});
+    })
+});
+
+
+
+router.get('/:id', (req, res) => {
+    Controller.getJourneys(req.params.id).then(data => {
+        res.status(data.status).send({data: data.data});
+    }).catch(err => {
+        res.status(err.status).send({message: err.message});
+    });
+});
+
+
+
+
+module.exports = router;
