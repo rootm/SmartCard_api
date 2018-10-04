2ca756d41b0f210b27c9d8e049cfd90b8a6e2bd8
rootm <muvindu.t@gmail.com>
2018-10-04 12:00:57 +0530

    routes endPoint added to route list

--- Routes.js
+++ Routes.js
@@ -2,10 +2,10 @@ var express =require('express');
 var AccountRoute = require('./src/Account/Account.Route');
 var CardRoute = require('./src/Card/CardDetails.Route');
 var JourneyRoute = require('./src/Journey/Journey.Route');
-
+var BusRoute = require('./src/Route/Route.Route');
 const Routes = express.Router();
 Routes.use('/accounts/',AccountRoute);
 Routes.use('/card/',CardRoute);
 Routes.use('/journey/', JourneyRoute);
-
+Routes.use('/routes/', BusRoute);
 module.exports =Routes;
